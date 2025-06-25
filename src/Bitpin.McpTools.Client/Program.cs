b361206684b90ca181985cd7b65dfce5c0ecca89
Kamyab <valipourkamyab.official@gmail.com>
2025-06-25 12:07:08 +0330

    feat: Add initial implementation for Bitpin.McpTools.Client project

---
+++ src/Bitpin.McpTools.Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using ModelContextProtocol.Client;
+
+var clientTransport = new StdioClientTransport(new()
+{
+    Name = "Bitpin Mcp Server",
+    Command = "dotnet",
+    Arguments = ["run", "--project", "C:\\Users\\Kamyab\\Desktop\\bitpin-mcp-server\\src\\Bitpin.McpServer\\Bitpin.McpServer.csproj", "--no-build"],
+    EnvironmentVariables = new Dictionary<string, string?> {
+        {"BITPIN_API_KEY","Test" },
+        {"BITPIN_API_SECRET","Test" },
+        {"BITPIN_API_URL","https://api.bitpin.org/api/v1/" },
+    }
+});
+
+
+
+await using var mcpClient = await McpClientFactory.CreateAsync(clientTransport);
+
+var tools = await mcpClient.ListToolsAsync();
+foreach (var tool in tools)
+{
+    Console.WriteLine($"Connected to server with tools: {tool.Name}");
+}
